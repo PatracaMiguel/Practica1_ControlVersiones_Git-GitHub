1f91f57b62923ee01b5c0ac62651dd2cc5a6d75a
Miguel Angel Patraca Lagunes <miguelangelpatracalagunes@gmail.com>
2026-02-22 07:18:04 -0600

    Update Tarea.java

--- TodoListApp/src/Tarea.java
+++ TodoListApp/src/Tarea.java
@@ -73,7 +73,7 @@ public class Tarea {
 
             tareaExiste = true;
 
-            guardarArchivo(); // importante para persistencia
+            guardarArchivo(); 
 
             break;
         }
